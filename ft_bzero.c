b5033b8fa3a8ca21ea8e322164cf8139ccb83813
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-12-28 20:28:51 +0000

    finished bzero and memset

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,25 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/24 14:26:06 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/28 20:28:27 by aantonio         ###   ########.fr       */
+/*   Created: 2022/12/28 20:24:05 by aantonio          #+#    #+#             */
+/*   Updated: 2022/12/28 20:25:22 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
 
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
-void	*ft_memset(void *s, int c, int n);
-void	*ft_bzero(void *s, int n);
-#endif
+void	bzero(void *s, int n)
+{
+	return (ft_memset(s, 0, n));
+}
--- ft_memset.c
+++ ft_memset.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/24 14:26:06 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/28 20:28:27 by aantonio         ###   ########.fr       */
+/*   Created: 2022/12/28 20:10:35 by aantonio          #+#    #+#             */
+/*   Updated: 2022/12/28 20:23:17 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+void	*memset(void *s, int c, int n)
+{
+	int	*intptr;
+	int	i;
 
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
-void	*ft_memset(void *s, int c, int n);
-void	*ft_bzero(void *s, int n);
-#endif
+	intptr = (int *)s;
+	i = 0;
+	while (i < n)
+	{
+		*(intptr + i) = c;
+	}
+	return (intptr);
+}
