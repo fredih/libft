4c6fb1d5c02ae0e8d69d9382e38f0191287d51f3
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-12-29 17:05:38 +0000

    finished ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 15:53:54 by aantonio          #+#    #+#             */
+/*   Updated: 2022/12/29 17:03:30 by aantonio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, unsigned int len)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!little)
+		return ((char *)big);
+	i = 0;
+	while (i < (unsigned int)ft_strlen(big) && i < len)
+	{
+		j = 0;
+		while (big[i + j] == little[j] && i + j < len)
+		{
+			if (little[j + 1] == 0)
+			{
+				return ((char *)big + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 14:26:06 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/29 15:47:11 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/12/29 16:11:02 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,6 @@ void			ft_memcpy(void *dest, void *src, unsigned int n);
 void			ft_memmove(void *dest, void *src, unsigned int n);
 int				ft_memcmp(const void *s1, const void *s2, unsigned int n);
 int				toupper(int c);
+int				tolower(int c);
+char			*ft_strnstr(const char *big, const char *lil, unsigned int len);
 #endif
