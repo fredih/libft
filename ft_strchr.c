4117613b867bc6f513e91f045ee10dcb0a871935
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-12-29 15:01:53 +0000

    fix norminette for ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 10:01:58 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/29 10:14:49 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/12/29 15:01:28 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	while (i < ft_strlen(s)+1)
+	while (i < ft_strlen(s) + 1)
 	{
 		if (s[i] == c)
 			return ((char *)s + i);
