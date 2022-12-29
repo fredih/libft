f7fe34855779580bbed861a6bff93b656bbd16a1
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-12-29 15:11:49 +0000

    fix unsigned bugs

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,15 +6,15 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 20:30:15 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/28 20:55:57 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/12/29 15:05:16 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_memcpy(void *dest, void *src, unsigned int n)
 {
-	int		i;
-	char	*chardest;
-	char	*charsrc;
+	unsigned int		i;
+	char		*chardest;
+	char		*charsrc;
 
 	chardest = (char *)dest;
 	charsrc = (char *)src;
--- ft_memset.c
+++ ft_memset.c
@@ -6,14 +6,14 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 20:10:35 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/28 20:54:09 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/12/29 15:06:03 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memset(void *s, int c, unsigned int n)
 {
-	char	*charptr;
-	int		i;
+	char				*charptr;
+	unsigned int		i;
 
 	charptr = (char *)s;
 	i = 0;
