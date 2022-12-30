109e9ce599265fef4c7b13551428ebd7d049f1d4
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-12-30 10:03:45 +0000

    add include for stdlib for malloc to work

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,11 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 09:45:36 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/30 09:57:29 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/12/30 10:03:32 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include <stdlib.h>
 #include "libft.h"
 
 void	*calloc(size_t nmemb, size_t size)
