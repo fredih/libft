b266d0e8d9103f987b1d5af6cb58c2dfb695a42c
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-12-30 10:12:30 +0000

    safeguar memset for null ptr parameter

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 20:10:35 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/30 09:55:14 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/12/30 10:12:04 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memset(void *s, int c, size_t n)
 	char				*charptr;
 	unsigned int		i;
 
+	if (!s)
+		return (0);
 	charptr = (char *)s;
 	i = 0;
 	while (i < n)
