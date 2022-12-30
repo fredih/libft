0dd2bc2e6167252d47814f8da80f87cf4112ae57
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-12-30 14:55:32 +0000

    fix strdup return value and switch to calloc

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,20 +6,22 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 10:02:00 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/30 10:26:08 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/12/30 14:55:16 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char	*strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
 	char	*newstr;
 	int		len;
 
 	len = ft_strlen(s);
-	newstr = malloc(len + 1);
+	newstr = ft_calloc(len + 1, sizeof(char));
+	if (!newstr)
+		return (0);
 	ft_strlcpy(newstr, s, len);
 	return (newstr);
 }
