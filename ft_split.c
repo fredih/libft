63128ba8a0e7999ba0fa8ce539a81db11e97bc4d
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2023-01-05 17:30:24 +0000

    almost finish split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 12:13:44 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/30 13:09:57 by aantonio         ###   ########.fr       */
+/*   Updated: 2023/01/05 17:27:29 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static int	count_chars(char const*s, char c)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] == c && s[i + 1] != c)
 			count++;
 		i++;
 	}
@@ -34,24 +34,35 @@ char	**ft_split(char const *s, char c)
 	char	**result;
 	int		wordcount;
 	char	*end;
+	char	*words;
+	char	*trimmed;
 	int		i;
 
-	s = ft_strtrim(s, &c);
-	wordcount = count_chars(s, c) + 1;
-	result = ft_calloc(wordcount, sizeof(char *));
+	if (!s)
+		return (0);
+	trimmed = ft_strtrim(s, &c);
+	if (!s[0])
+	{
+		result = ft_calloc(1, sizeof(char *));
+		return (result);
+	}
+	wordcount = count_chars(trimmed, c) + 1;
+	result = ft_calloc(wordcount + 1, sizeof(char *));
 	if (!result)
 		return (NULL);
 	i = 0;
+	words = trimmed;
 	while (i < wordcount)
 	{
-		end = ft_strchr(s, c);
+		end = ft_strchr(words, c);
 		if (!end)
 			break ;
-		result[i] = ft_substr(s, 0, end - s);
-		s = ft_substr(s, end - s + 1, ft_strlen(s));
-		i++;
+		result[i++] = ft_substr(words, 0, end - words);
+		while (*end == c)
+			end++;
+		words = end;
 	}
-	free(end);
-	result[i] = (char *)s;
+	result[i] = ft_substr(words, 0, ft_strlen(words));
+	free(trimmed);
 	return (result);
 }
