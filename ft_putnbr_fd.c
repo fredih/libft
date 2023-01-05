4420acfbecf7bf688cfe5531a72bf605a3bf05f8
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2023-01-05 14:33:29 +0000

    finished putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,16 +6,38 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 15:24:59 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/30 16:34:49 by aantonio         ###   ########.fr       */
+/*   Updated: 2023/01/05 14:33:10 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	put_digit(unsigned int n, int fd)
+{
+	if (n >= 10)
+	{
+		put_digit(n / 10, fd);
+		ft_putchar_fd(n % 10 + '0', fd);
+	}
+	if (n < 10)
+	{
+		ft_putchar_fd(n % 10 + '0', fd);
+	}
+	return (n);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*s;
+	unsigned int	uint_n;
 
-	s = ft_itoa(n);
-	ft_putstr_fd(s, fd);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		uint_n = -n;
+	}
+	else
+	{
+		uint_n = n;
+	}
+	put_digit(uint_n, fd);
 }
