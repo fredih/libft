f075b2b0e09a1fcb1efa29c2ceefec08ae57b009
Alfredo Antonio Hernandez Inostroza <aantonio@c1r17s2.42lisboa.com>
2023-01-06 23:24:10 +0000

    fixed atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,44 +6,13 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 16:03:50 by aantonio          #+#    #+#             */
-/*   Updated: 2022/12/30 10:25:26 by aantonio         ###   ########.fr       */
+/*   Updated: 2023/01/06 22:55:25 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-
-static int	pwr10(int exponent)
-{
-	int	i;
-	int	result;
-
-	result = 1;
-	i = 0;
-	while (i < exponent)
-	{
-		result *= 10;
-		i++;
-	}
-	return (result);
-}
-
-static int	get_number(char *start, int pwr)
-{	
-	int	i;
-	int	number;
-
-	number = 0;
-	i = 0;
-	while (pwr != 0)
-	{
-		number += (*(start + i) - '0') * pwr;
-		pwr /= 10;
-		i++;
-	}
-	return (number);
-}
 
 static int	ft_isspace(char c)
 {
@@ -58,46 +27,27 @@ static int	ft_isspace(char c)
 	return (0);
 }
 
-int	getsign(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (!('0' <= str[i] && str[i] <= '9'))
-	{
-		i++;
-	}
-	if (str[i - 1] == '-')
-		return (-1);
-	else
-		return (1);
-}
-
 int	ft_atoi(const char *str)
 {
-	int		i;
-	char	*start;
-	char	*end;
-	int		number;
-	int		pwr;
+	int	i;
+	int	res;
+	int	signal;
 
+	signal = 1;
+	res = 0;
 	i = 0;
 	while (ft_isspace(str[i]))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
-		i++;
-	if (!('0' <= str[i] && str[i] <= '9'))
-		return (0);
-	while ('0' == str[i] && str[i + 1] == '0')
-		i++;
-	start = (char *)str + i;
-	i++;
-	while ('0' <= str[i] && str[i] <= '9')
 	{
+		if (str[i] == '-')
+			signal = -signal;
 		i++;
 	}
-	end = (char *)str + i;
-	pwr = pwr10((int)(end - start - 1));
-	number = get_number(start, pwr);
-	return (getsign(str) * number);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * signal);
 }
